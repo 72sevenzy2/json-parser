292e78b1cd7580dd0fda6697e637b8ade646e698
72 <72sevenzy2@gmail.com>
2026-04-30 15:02:32 +0500

    refactor/docs: refine comments for both Failed and Ok functions purpose

--- helpers/options.go
+++ helpers/options.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 )
 
-// quick method for responses without needing to throw in any paramters (except the data needed and the http.responseWriter) as options for if you were to actually use the JSON(...) func.
+// Ok helper. -> pass in a http.ResponseWriter, and some data. Example: map[string]string{ "User": "Name" }
 func Ok(w http.ResponseWriter, data any) {
 	response.JSON(w, response.WithData(data), response.WithStatus(http.StatusOK))
 }
 
-// a method for failed aswell. (though status and msg is both needed.)
+// Failed helper. -> pass in a http.ResponseWriter, a status code (int), and a message to display to the user (string).
 func Failed(w http.ResponseWriter, status int, msg string) {
 	response.JSON(w, response.WithStatus(status), response.WithError(msg))
 }
